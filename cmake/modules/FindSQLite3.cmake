c6f67508a33a935a34de1a8bf7d0f28c2a4dcf03
NikitaFeodonit <nfeodonit@yandex.com>
2018-08-23 12:53:11 +0300

    Patch 'FindSQLite3.cmake'.

--- cmake/modules/FindSQLite3.cmake
+++ cmake/modules/FindSQLite3.cmake
@@ -16,14 +16,26 @@
 # Find the native SQLITE3 headers and libraries.
 #
 # SQLITE3_INCLUDE_DIRS  - where to find sqlite3.h, etc.
-# SQLITE3_LIBRARIES - List of libraries when using sqlite.
-# SQLITE3_FOUND - True if sqlite found.
+# SQLITE3_LIBRARIES     - List of libraries when using sqlite.
+# SQLITE3_FOUND         - True if sqlite found.
 
 # Look for the header file.
-find_path(SQLITE3_INCLUDE_DIR NAMES sqlite3.h)
+find_path(SQLITE3_INCLUDE_DIR
+  NAMES sqlite3.h
+  PATH_SUFFIXES include
+  NO_CMAKE_ENVIRONMENT_PATH
+  NO_SYSTEM_ENVIRONMENT_PATH
+  NO_CMAKE_SYSTEM_PATH
+)
 
 # Look for the library.
-find_library(SQLITE3_LIBRARY NAMES sqlite)
+find_library(SQLITE3_LIBRARY
+  NAMES sqlite3 sqlite3d
+  PATH_SUFFIXES lib
+  NO_CMAKE_ENVIRONMENT_PATH
+  NO_SYSTEM_ENVIRONMENT_PATH
+  NO_CMAKE_SYSTEM_PATH
+)
 
 # Handle the QUIETLY and REQUIRED arguments and set SQLITE3_FOUND to TRUE
 # if all listed variables are TRUE.
@@ -41,4 +53,4 @@ else(SQLITE3_FOUND)
   set(SQLITE3_INCLUDE_DIRS)
 endif(SQLITE3_FOUND)
 
-mark_as_advanced(SQLITE3_INCLUDE_DIRS SQLITE3_LIBRARIES)
+mark_as_advanced(SQLITE3_INCLUDE_DIR SQLITE3_LIBRARY)
