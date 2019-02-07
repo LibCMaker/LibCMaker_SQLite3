3f7c379e9fc4e66dabbd4ffe7d6a468dd12fbf8b
NikitaFeodonit <nfeodonit@yandex.com>
2019-02-07 14:27:02 +0300

    Add the building with Travis CI.

---
+++ cmr_build_sqlite3.cmake
@@ -0,0 +1,98 @@
+# ****************************************************************************
+#  Project:  LibCMaker_SQLite3
+#  Purpose:  A CMake build script for SQLite3 library
+#  Author:   NikitaFeodonit, nfeodonit@yandex.com
+# ****************************************************************************
+#    Copyright (c) 2017-2019 NikitaFeodonit
+#
+#    This file is part of the LibCMaker_SQLite3 project.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published
+#    by the Free Software Foundation, either version 3 of the License,
+#    or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#    See the GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+# ****************************************************************************
+
+#-----------------------------------------------------------------------
+# Lib's name, version, paths
+#-----------------------------------------------------------------------
+
+set(SQLITE3_lib_NAME      "SQLite3")
+set(SQLITE3_lib_VERSION   "3.24.0")
+set(SQLITE3_lib_DIR       "${CMAKE_CURRENT_LIST_DIR}")
+
+# To use our Find<LibName>.cmake.
+list(APPEND CMAKE_MODULE_PATH "${SQLITE3_lib_DIR}/cmake/modules")
+
+
+#-----------------------------------------------------------------------
+# LibCMaker_<LibName> specific vars and options
+#-----------------------------------------------------------------------
+
+set(COPY_SQLITE3_CMAKE_BUILD_SCRIPTS ON)
+
+
+#-----------------------------------------------------------------------
+# Library specific vars and options
+#-----------------------------------------------------------------------
+
+#option(BUILD_SHARED_LIBS "build shared library" ${BUILD_SHARED_LIBS})
+option(SQLITE_BUILD_SHELL "Build the sqlite3 shell" OFF)
+
+option(SQLITE_THREADSAFE "build a thread-safe library" ON)
+option(SQLITE_OMIT_LOAD_EXTENSION
+  "omits the entire extension loading mechanism" ON
+)
+
+option(SQLITE_ENABLE_FTS3
+  "version 3 of the full-text search engine, see also SQLITE_ENABLE_FTS4 which enables FTS3 as well"
+  OFF
+)
+option(SQLITE_ENABLE_FTS3_PARENTHESIS
+  "modifies the query pattern parser in FTS3 such that it supports operators AND and NOT and also allows query expressions to contain nested parenthesis"
+  OFF
+)
+option(SQLITE_ENABLE_FTS4
+  "versions 3 and 4 of the full-text search engine" OFF
+)
+option(SQLITE_ENABLE_FTS5 "versions 5 of the full-text search engine" OFF)
+option(SQLITE_ENABLE_ICU "the ICU extension to SQLite to be added" OFF)
+option(SQLITE_ENABLE_JSON1
+  "the JSON SQL functions are added to the build automatically" OFF
+)
+option(SQLITE_ENABLE_RTREE "include support for the R*Tree index extension" OFF)
+option(SQLITE_ENABLE_COLUMN_METADATA
+  "include APIs that provide access to meta-data about tables and queries"
+  OFF
+)
+
+option(SQLITE_DEBUG "Build SQLite debug features" OFF)
+option(SQLITE_MEMDEBUG "Build SQLite memory debug features" OFF)
+
+
+#-----------------------------------------------------------------------
+# Build, install and find the library
+#-----------------------------------------------------------------------
+
+if(SQLITE_ENABLE_ICU)
+  # Used in 'cmr_build_rules_sqlite3.cmake'.
+  set(LIBCMAKER_ICU_SRC_DIR ${ICU_lib_DIR})
+endif()
+
+cmr_find_package(
+  LibCMaker_DIR   ${LibCMaker_DIR}
+  NAME            ${SQLITE3_lib_NAME}
+  VERSION         ${SQLITE3_lib_VERSION}
+  LIB_DIR         ${SQLITE3_lib_DIR}
+  REQUIRED
+  CONFIG
+  NOT_USE_VERSION_IN_FIND_PACKAGE
+)
---
+++ samples/TestCompileWithSQLite3/src/example.cpp
@@ -0,0 +1,77 @@
+/*****************************************************************************
+ * Project:  LibCMaker_SQLite3
+ * Purpose:  A CMake build script for SQLite3 library
+ * Author:   NikitaFeodonit, nfeodonit@yandex.com
+ *****************************************************************************
+ *   Copyright (c) 2017-2019 NikitaFeodonit
+ *
+ *    This file is part of the LibCMaker_SQLite3 project.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published
+ *    by the Free Software Foundation, either version 3 of the License,
+ *    or (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *    See the GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program. If not, see <http://www.gnu.org/licenses/>.
+ ****************************************************************************/
+
+// The code is from:
+// http://zetcode.com/db/sqlitec/
+
+#include <sqlite3.h>
+#include <stdio.h>
+
+int main(void) {
+
+  sqlite3 *db;
+  char *err_msg = 0;
+
+  int rc = sqlite3_open(":memory:", &db);
+
+  if (rc != SQLITE_OK) {
+
+    fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
+    sqlite3_close(db);
+
+    return 1;
+  }
+
+  char *sql = "CREATE TABLE Friends(Id INTEGER PRIMARY KEY, Name TEXT);"
+              "INSERT INTO Friends(Name) VALUES ('Tom');"
+              "INSERT INTO Friends(Name) VALUES ('Rebecca');"
+              "INSERT INTO Friends(Name) VALUES ('Jim');"
+              "INSERT INTO Friends(Name) VALUES ('Roger');"
+              "INSERT INTO Friends(Name) VALUES ('Robert');";
+
+  rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
+
+  if (rc != SQLITE_OK) {
+
+    fprintf(stderr, "Failed to create table\n");
+    fprintf(stderr, "SQL error: %s\n", err_msg);
+    sqlite3_free(err_msg);
+    return 1;
+
+  } else {
+
+    fprintf(stdout, "Table Friends created successfully\n");
+  }
+
+  int last_id = sqlite3_last_insert_rowid(db);
+  printf("The last Id of the inserted row is %d\n", last_id);
+
+  if (last_id != 5) {
+    printf("ERROR: the last Id of the inserted row is NOT 5\n");
+    return 1;
+  }
+
+  sqlite3_close(db);
+
+  return 0;
+}
