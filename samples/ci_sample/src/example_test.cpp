d0995df65bcdc8bcf4bc3e6862a87b5962be5ca8
NikitaFeodonit <nfeodonit@yandex.com>
2022-06-03 12:57:07 +0300

    Fix warning of C++11 in example.cpp and example_test.cpp

--- samples/ci_sample/src/example_test.cpp
+++ samples/ci_sample/src/example_test.cpp
@@ -44,7 +44,7 @@ TEST(Examle, test)
     EXPECT_EQ(rc, SQLITE_OK);
   }
 
-  char *sql = "CREATE TABLE Friends(Id INTEGER PRIMARY KEY, Name TEXT);"
+  const char *sql = "CREATE TABLE Friends(Id INTEGER PRIMARY KEY, Name TEXT);"
       "INSERT INTO Friends(Name) VALUES ('Tom');"
       "INSERT INTO Friends(Name) VALUES ('Rebecca');"
       "INSERT INTO Friends(Name) VALUES ('Jim');"
